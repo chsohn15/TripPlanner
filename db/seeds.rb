318cd70bd7ff4391c49f4797639cff96d28892c8
slee713 <steven.lee1393@gmail.com>
2020-08-13 14:50:51 -0400

    Created Country/State/City data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,17 @@
 # Use CS to create countries, regions and cities.
 
 # Use FAKER to generate random locations. 
+Country.destroy_all
+State.destroy_all
+City.destroy_all
+
+
+CS.countries.each do |c_abbr,country_name|
+    Country.create(name: country_name)
+    CS.states(c_abbr).each do |s_abbr, state_name|
+        State.create(name: state_name, country: Country.find_by(name: country_name))
+        CS.cities(s_abbr, c_abbr).each do |city_name|
+            City.create(name: city_name, state: State.find_by(name: state_name))
+        end
+    end
+end
