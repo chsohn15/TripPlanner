f39499fa3d06a5dd3103b278d311fc591d67a5fe
solomonfy <fantahunsolomon@gmail.com>
2020-08-14 14:18:56 -0400

    more on add location

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,6 +1,10 @@
 class CountriesController < ApplicationController
+
   def index
     @countries = Country.all
+    @states = State.where(country_id: params[:country])
+    @cities = City.where(state_id: params[:state])
+    @locations = Location.where(city_id: params[:city])
   end
   
 end
