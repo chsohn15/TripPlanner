6170b2828b56f22097249e12b19cafea913e0eaf
slee713 <steven.lee1393@gmail.com>
2020-08-17 11:57:19 -0400

    created users and some formatting fixes

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_163911) do
+ActiveRecord::Schema.define(version: 2020_08_17_155219) do
 
   create_table "cities", force: :cascade do |t|
     t.string "name"
     t.integer "state_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "description"
   end
 
   create_table "countries", force: :cascade do |t|
@@ -55,6 +56,15 @@ ActiveRecord::Schema.define(version: 2020_08_13_163911) do
     t.date "start_date"
     t.date "end_date"
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
