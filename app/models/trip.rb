604da2e0df913c9c0b42df2277b1879b6c756908
solomonfy <fantahunsolomon@gmail.com>
2020-08-20 17:04:06 -0400

    forms and alignments done

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,13 +3,14 @@ class Trip < ApplicationRecord
     has_many :trip_locations
     has_many :locations, through: :trip_locations
     validates :name, :start_date, :end_date, presence: true
+    validates :name, uniqueness: {case_sensitive: false}
     validate :end_date_is_after_start_date
 
 
     def average_rating
         trip_location = TripLocation.where(trip_id: self.id) 
         if trip_location.any?
-            trip_location.pluck(:rating).sum/trip_location.count.to_f
+            (trip_location.pluck(:rating).sum/trip_location.count.to_f).round(2)
         else
             0
         end
