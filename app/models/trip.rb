1bbc8e7df21c65899e0cc7dc85c9cfb6ccca6645
slee713 <steven.lee1393@gmail.com>
2020-08-20 12:50:49 -0400

    aligned text in cards

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -3,6 +3,7 @@ class Trip < ApplicationRecord
     has_many :trip_locations
     has_many :locations, through: :trip_locations
     validates :name, :start_date, :end_date, presence: true
+    validate :end_date_is_after_start_date
 
 
     def average_rating
@@ -16,5 +17,14 @@ class Trip < ApplicationRecord
 
     def unique_cities_visited
         self.locations.map {|location| location.city}.uniq 
+    end
+
+
+    private 
+
+    def end_date_is_after_start_date
+        if end_date < start_date
+            errors.add(:end_date, "cannot be before the start date")
+        end
     end
 end
