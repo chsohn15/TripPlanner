4aafe50066aca0561e0c5e8b8ee4c7cc29137749
solomonfy <fantahunsolomon@gmail.com>
2020-08-21 11:30:14 -0400

    almost done

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -3,6 +3,11 @@ class CountriesController < ApplicationController
   def index
     @countries = Country.all
   end
+
   def about
+    @most_visited_country = Country.most_visited_country.name
+    @most_visited_city = "#{City.most_visited_city.name}, #{City.most_visited_city.state.name}"
   end
+
+
 end
